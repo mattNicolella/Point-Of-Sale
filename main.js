677ec9e0a0bc83162bd3dd6cb97539967710ef6e
Matthew Nicolella <sp1k3677@gmail.com>
2023-01-10 18:54:34 -0500

    mi chonclas

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ app.whenReady().then(() => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 
-    ipcMain.handle('', handleFileOpen)
+     //ipcMain.handle('', handleFileOpen)
 });
 
 app.on('window-all-closed', () => {
