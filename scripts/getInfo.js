ee8b83be19d7f94912f5a51253410b363914a998
Matthew Nicolella <sp1k3677@gmail.com>
2023-01-12 16:43:02 -0500

    Started Adding Customers

--- scripts/getInfo.js
+++ scripts/getInfo.js
@@ -3,10 +3,7 @@ const {execSync} = require('child_process');
 module.exports = {
     name: 'getInfo',
     execute(payload, ...args) {
-        //console.log('type:', args);
         const output = execSync(`php ./php/${args.shift()}.php `+args.join(" ")).toString();
-        //console.log(output);
-
         return output;
     },
 };
